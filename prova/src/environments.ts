9537d4d345417688d0699b6e05d86bbb19fc0d3f
LoryMorty <168710437+LoryMorty@users.noreply.github.com>
2025-11-25 10:39:45 +0000

    scheletro fatto,sviluppare il log-in

---
+++ prova/src/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+firebase: {
+apiKey: "AIzaSyC8HNCjuv5frtfuvuYwK9d96dFBHISZ6rk",
+authDomain: "prova-2fc3a.firebaseapp.com",
+projectId: "prova-2fc3a",
+storageBucket: "prova-2fc3a.firebasestorage.app",
+messagingSenderId: "10160378680",
+appId: "1:10160378680:web:4eb76ec3577046b227e7cd",
+measurementId: "G-VWPNE9FTGC"
+}
+};
